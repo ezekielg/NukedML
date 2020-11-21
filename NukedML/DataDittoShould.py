76a3c93269abae0f29145bf5eccb626522578d51
Ezekiel Gomez <ezekielg@uw.edu>
2020-11-20 22:04:07 -0800

    Initial upload

---
+++ NukedML/DataDittoShould.py
@@ -0,0 +1,44 @@
+import datetime
+import pandas as pd
+import pprint as pp
+
+from NukedML.Utilities.DataDitto import Faker
+
+def test_faker_class():
+    df_json = {
+        'column_1': ['Apple', 'ApPlE', 'banana', 'BANANA', 'Banana', 'Orange', 'Apple', 'APPLE', 'Cherry', 'Cherry'],
+        'column_2': [1, 2, 3, -1, -2, -3, 4, 6, 1, -1],
+        'column_3': [True, True, True, False, True, False, False, False, False, False],
+        'column_4': [1.0, 2.0, 3.0, -3.14, -3.14, -42.69, -69.42, -69.42, -100.1, 99],
+        'column_5': ['2017-03-25 15:16:45', '2017-03-25 15:16:45', '2017-03-19 15:16:45', '2017-03-22 15:16:45',
+                     '2017-03-22 15:16:45', '2017-03-19 15:16:45', '2017-03-30 15:16:45', '2017-03-22 15:16:45',
+                     '2017-03-30 15:16:45', '2017-03-25 15:16:45']
+    }
+
+    for i, _ in enumerate(df_json['column_5']):
+        df_json['column_5'][i] = datetime.datetime.strptime(df_json['column_5'][i], '%Y-%m-%d %H:%M:%S')
+
+    df = pd.DataFrame(df_json)
+    fakey = Faker.ditto_dataframe(df, size=100)
+
+    pp.pprint(fakey)
+
+
+def test_config_json():
+    test = {
+        'a': [1, 99],
+        'b': [-0.234, 9.431],
+        'c': [datetime.datetime.strptime('2020-04-20 04:20:00', '%Y-%m-%d %H:%M:%S'),
+              datetime.datetime.strptime('2020-05-05 05:15:42', '%Y-%m-%d %H:%M:%S')],
+        'x': ['apple', 'banana', 'OraNgE'],
+        'okay': [True, False]
+    }
+
+    f = Faker(config_json=test, seed=420)
+
+    pp.pprint(f.fabricate(size=100))
+
+
+if __name__ == '__main__':
+    test_faker_class()
+    test_config_json()
---
+++ NukedML/Utilities/DataDitto.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+
+
+class Faker:
+    def __init__(self, config_json, seed=6):
+        self._rng = np.random.RandomState(seed)
+        self._config_json = config_json
+        self._config = {k: pd.Series(v) for k, v in self._config_json.items()}
+
+    def fabricate(self, size=100):
+        faked_df = pd.DataFrame({})
+
+        for k, v in self._config.items():
+            if pd.api.types.is_integer_dtype(v):
+                faked_df[k] = self._rng.randint(low=v.min(), high=v.max()+1, size=size)
+            elif pd.api.types.is_float_dtype(v):
+                faked_df[k] = self._rng.uniform(low=v.min(), high=v.max(), size=size)
+            elif pd.api.types.is_string_dtype(v):
+                faked_df[k] = self._rng.choice(a=v, size=size)
+            elif pd.api.types.is_bool_dtype(v):
+                faked_df[k] = self._rng.choice(a=[True, False], size=size)
+            elif pd.api.types.is_datetime64_any_dtype(v):
+                dates = pd.date_range(start=v.min(), end=v.max(), periods=(size*1.25)//1)
+                faked_df[k] = self._rng.choice(a=dates, size=size)
+
+        return faked_df[sorted(faked_df)]
+
+    @staticmethod
+    def ditto_dataframe(df, size=100, seed=6):
+        config_json = {}
+
+        for c in sorted(df.select_dtypes(include=['number', 'datetime', 'datetimetz'])):
+            config_json[c] = [df[c].min(), df[c].max()]
+
+        for c in sorted(df.select_dtypes(include=['object'])):
+            config_json[c] = df[c].apply(str.upper).unique().tolist()  # Do we really want to uppercase here?
+
+        for c in sorted(df.select_dtypes(include=['bool'])):
+            config_json[c] = [True, False]
+
+        dittoer = Faker(config_json=config_json, seed=seed)
+        faked_df = dittoer.fabricate()
+
+        return faked_df[sorted(faked_df)]
+
+    @staticmethod
+    def ditto_series(*args, **kwargs):
+        print("Don't be silly, just use sampling.")
